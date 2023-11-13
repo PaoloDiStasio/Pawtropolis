10b0cc9acaff83d3b36c6823a82e03228058e5d7
pdistasio <pdistasio@20230123-AIT.prod.altengroup.dir>
2023-11-13 10:15:42 +0100

    add map for store all the animals. add function getAnimal for get animals from map by class. edit the function returnRecordAnimal for all the type of animals.

--- Zoo/src/Main.java
+++ Zoo/src/Main.java
@@ -13,39 +13,41 @@ public class Main {
 		Tiger tigress = new Tiger("Tigress", "tacchino", 15, LocalDate.of(2023, 10, 31), 80,(float) 1.70, (float) 1.5);
 
 		
-		Lion leone = new Lion("leone", "pollo", 11,  LocalDate.of(2020, 5, 10), 50, (float) 1.80, 1);
+		Lion leone = new Lion("leone", "pollo", 11,  LocalDate.of(2020, 5, 10), 50, (float) 1.80, (float) 2.1);
 		Lion leo = new Lion("leo", "capra", 8, LocalDate.of(2019, 10, 5), 60, (float) 1.75, (float) 1.5);
 		
 		
 		Eagle aquilotto = new Eagle("Aquilotto", "verme", 5,  LocalDate.of(2020, 5, 10), 5, (float) 0.8, (float) 2.3);
 		Eagle lazio = new Eagle("Lazio", "uccelli", 10, LocalDate.of(2010, 10, 11), 6, (float) 1, (float) 2.5);
 
-				
+						
 		System.out.println("Zoo");
 		
 		ZooController zooController = new ZooController();
 		
-		zooController.addAnimalWithTail(leone);
-		zooController.addAnimalWithTail(tigro);
-		zooController.addAnimalWithTail(tigress);
-		zooController.addAnimalWithTail(leo);
-		zooController.addAnimalWithWings(aquilotto);
-		zooController.addAnimalWithWings(lazio);
+		zooController.addAnimal(Lion.class, leone);
+		zooController.addAnimal(Lion.class, leo);
+		zooController.addAnimal(Tiger.class, tigro);
+		zooController.addAnimal(Tiger.class, tigress);
+		zooController.addAnimal(Eagle.class, lazio);
+		zooController.addAnimal(Eagle.class, aquilotto);
 
-		BiPredicate<Animal, Animal> heaviestTiger = (tiger1, tiger2) -> (tiger2 == null && tiger1 instanceof Tiger) || tiger2 != null && tiger1 instanceof Tiger && tiger1.getWeight() > tiger2.getWeight();
-		System.out.println("La tigre più pesante è " + zooController.returnRecordAnimal(heaviestTiger).getName() + " peso: " + zooController.returnRecordAnimal(heaviestTiger).getWeight() + " kg"); 
+		System.out.println("Animali con la coda: "+ zooController.getAnimal(AnimalWithTail.class));
+		System.out.println("Animali con le ali: "+ zooController.getAnimal(AnimalWithWings.class));
+		System.out.println("Leoni: "+ zooController.getAnimal(Lion.class));
+
 		
-		BiPredicate<Animal, Animal> lightestLion = (lion1, lion2) -> (lion2 == null && lion1 instanceof Lion) || lion2 != null && lion1 instanceof Lion && lion1.getWeight() < lion2.getWeight();
-		System.out.println("Il lione più leggero è " + zooController.returnRecordAnimal(lightestLion).getName() + " peso: " + zooController.returnRecordAnimal(lightestLion).getWeight() + " kg");
+		BiPredicate<Tiger, Tiger> heaviestTiger = (tiger1, tiger2) -> (tiger2 == null && tiger1 instanceof Tiger) || tiger2 != null && tiger1 instanceof Tiger && tiger1.getWeight() > tiger2.getWeight();
+		System.out.println("Tigre più pesante : " + zooController.returnRecordAnimal(heaviestTiger, Tiger.class).getName());
 		
-		BiPredicate<Animal, Animal> heaviestEagle = (eagle1, eagle2) -> (eagle2 == null && eagle1 instanceof Eagle) || eagle2 != null && eagle1 instanceof Eagle && eagle1.getWeight() > eagle2.getWeight();
-		System.out.println("L'aquila più pesante è " + zooController.returnRecordAnimal(heaviestEagle).getName() + " peso: " + zooController.returnRecordAnimal(heaviestEagle).getWeight() + " kg");
-		
+		BiPredicate<Eagle, Eagle> heaviestEagle = (eagle1, eagle2) -> (eagle2 == null && eagle1 instanceof Eagle) || eagle2 != null && eagle1 instanceof Eagle && eagle1.getWeight() > eagle2.getWeight();
+		System.out.println("L'aquila più pesante è: " + zooController.returnRecordAnimal(heaviestEagle, Eagle.class).getName());
+
 		BiPredicate<AnimalWithTail, AnimalWithTail> longestTail = (tail1, tail2) -> (tail2 == null && tail1 instanceof AnimalWithTail) || tail2 != null && tail1 instanceof AnimalWithTail && tail1.getTailLenght() > tail2.getTailLenght(); 
-		System.out.println("L'animale con la coda più lunga è " + zooController.returnRecordAnimalWithTail(longestTail).getName() + " lunghezza: " + zooController.returnRecordAnimalWithTail(longestTail).getTailLenght() + " m");
-
+		System.out.println("L'animale con la coda più lunga è: " + zooController.returnRecordAnimal(longestTail, AnimalWithTail.class).getName());
+		
 		BiPredicate<AnimalWithWings, AnimalWithWings> largestWingspan = (wing1, wing2) -> (wing2 == null && wing1 instanceof AnimalWithWings) || wing2 != null && wing1 instanceof AnimalWithWings && wing1.getWingspan() > wing2.getWingspan(); 
-		System.out.println("L'uccello con l'apertura alare più larga è " + zooController.returnRecordAnimalWithWings(largestWingspan).getName() + " lunghezza: " + zooController.returnRecordAnimalWithWings(largestWingspan).getWingspan() + " m");
+		System.out.println("L'uccello con l'apertura alare più larga è: " + zooController.returnRecordAnimal(largestWingspan, AnimalWithWings.class).getName());
 
 	}
 	
--- Zoo/src/ZooController.java
+++ Zoo/src/ZooController.java
@@ -1,67 +1,37 @@
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.BiPredicate;
+import java.util.stream.Collectors;
 
 public class ZooController {
-
-	ArrayList<Animal> animals = new ArrayList<>();
-	ArrayList<AnimalWithTail> animalsWithTail = new ArrayList<>();
-	ArrayList<AnimalWithWings> animalsWithWings = new ArrayList<>();
 	
-	public void addAnimalWithTail(AnimalWithTail animal) {
-		this.animalsWithTail.add(animal);
-		this.animals.add(animal);	
+	Map<Class<? extends Animal>, List<Animal>> animals = new HashMap<>(); 
+	
+	public void addAnimal(Class classname, Animal animal) {
+		animals.computeIfAbsent(classname, k -> new ArrayList<>()).add(animal);
 	}
 	
-	public void addAnimalWithWings(AnimalWithWings animal) {
-		this.animalsWithWings.add(animal);
-		this.animals.add(animal);
-	}
-
-	public ArrayList<Animal> getAnimals(){
-		return animals;
-	}
-	
-	public ArrayList<AnimalWithTail> getAnimalsWithTail(){
-		return animalsWithTail;
-	}
-	
-	public ArrayList<AnimalWithWings> getAnimalsWithWings(){
-		return animalsWithWings;
-	}
-
-	public Animal returnRecordAnimal(BiPredicate<Animal, Animal> compareAnimals){
-		Animal recordAnimal = null;
-		
-		for(Animal animal : animals){
-			if(compareAnimals.test(animal, recordAnimal)){
-				recordAnimal = animal;
-			}
+	public <T extends Animal> List<T> getAnimal(Class<T> classname){
+		if (animals.get(classname) != null) {
+			return (List<T>) animals.get(classname);
 		}
-		
-		return recordAnimal;
+		return animals.values().stream()
+				.flatMap(Collection::stream)
+				.filter(classname::isInstance)
+				.map(classname::cast)
+				.collect(Collectors.toList());
 	}
-
-	public AnimalWithTail returnRecordAnimalWithTail(BiPredicate<AnimalWithTail, AnimalWithTail> compareAnimals){
-		AnimalWithTail recordAnimal = null;
+	
+	
+	public <T extends Animal> T returnRecordAnimal(BiPredicate<T, T> compareAnimals, Class<T> classname){
+		T recordAnimal = null;
 		
-		for(AnimalWithTail animal : animalsWithTail){
-			if(compareAnimals.test(animal, recordAnimal)){
-				recordAnimal = animal;
-			}
-		}
-		
-		return recordAnimal;
-	}
-
-
-	public AnimalWithWings returnRecordAnimalWithWings(BiPredicate<AnimalWithWings, AnimalWithWings> compareAnimals){
-		AnimalWithWings recordAnimal = null;
-		
-		for(AnimalWithWings animal : animalsWithWings){
+		for(T animal : getAnimal(classname)){
 			if(compareAnimals.test(animal, recordAnimal)){
 				recordAnimal = animal;
 			}
